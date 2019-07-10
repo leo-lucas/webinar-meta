e718e883352221796c1cbb3901f71741fe98b949
Leonardo <leonardo.santos@meta.com.br>
2019-07-10 16:49:08 -0300

    ajustes de configuração

--- js/src/main.js
+++
@@ -1,5 +0,0 @@
-// tipos
-// Funções
-// Objetos
-// Classes
-// Enuns
--- js/src/main.ts
+++ js/src/main.ts
@@ -3,3 +3,9 @@
 // Objetos
 // Classes
 // Enuns
+console.log("teste")
+const teste: string= "teste"
+class Classjs{
+    constructor(public teste:string){
+    }
+}
