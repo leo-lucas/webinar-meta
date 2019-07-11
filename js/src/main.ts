41ccec90bc6386df5614aa937835ba4bb54f8ca5
Leonardo <leonardo.santos@meta.com.br>
2019-07-11 18:13:38 -0300

    adicionado passos

--- js/src/main.ts
+++ js/src/main.ts
@@ -1,11 +1,46 @@
-// tipos
+console.log("----------------------------------");
+
+// tipagem
+const string: string = "webinar";
+const numero = 1;
+
 // Funções
-// Objetos
-// Classes
-// Enuns
-console.log("teste")
-const teste: string= "teste"
-class Classjs{
-    constructor(public teste:string){
-    }
+function converterParaString(numero: number): string {
+  return numero.toString();
 }
+const type = converterParaString(3);
+console.log("Função converter", type, typeof type);
+
+// Objetos
+const objeto: { atributo1: string; atributo2?: number } = {
+  atributo1: "Meta",
+  atributo2: 0
+};
+
+// Interfaces
+interface UserInterface {
+  name: string;
+  idade: number;
+  toString: () => String;
+}
+
+// Classes
+class User {
+  constructor(public name: string) {}
+}
+
+console.log("class", new User("Leonardo"));
+
+class UserDois implements UserInterface {
+  constructor(public name: string, public idade: number) {
+    console.log("CL construtor", this.toString());
+  }
+  toString = () => this.name;
+}
+
+console.log("class", new UserDois("Leonardo", 20));
+
+// Enuns
+// Generics
+let valor: Array<String>;
+console.log("----------------------------------");
