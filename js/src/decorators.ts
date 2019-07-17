1cd4b7477eb9fff802b325ffe67901e602fe9d70
Leonardo <leonardo.santos@meta.com.br>
2019-07-17 18:48:17 -0300

    add decorator

---
+++ js/src/decorators.ts
@@ -0,0 +1,11 @@
+import "reflect-metadata";
+export function Logger<T extends { new (...args: any[]): {} }>(constructor: T) {
+  return class extends constructor {
+    constructor(...args: any[]) {
+      super(...args);
+    }
+    log() {
+      console.log(this);
+    }
+  };
+}
